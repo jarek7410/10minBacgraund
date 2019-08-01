b782d6a0b6110cc662462cc71a0ecbd0b02fe615
jarek7410 <jarekomsta@gmail.com>
2019-08-01 14:46:17 +0200

    --Let's Start--
    Signed-off-by: jarek7410 <jarekomsta@gmail.com>

--- src/sample/Chose.java
+++ src/sample/Chose.java
@@ -2,12 +2,10 @@ package sample;
 
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
