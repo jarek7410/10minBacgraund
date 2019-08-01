ad3b4ae3ba6c873f88e810738b5c7058eb6df901
jarek7410 <39216951+jarek7410@users.noreply.github.com>
2019-08-01 14:44:59 +0200

    --Let's Start--
    Signed-off-by: jarek7410 <jarekomsta@gmail.com>

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,6 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
